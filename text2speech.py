708f2b74141243c36351cd2f60f81a5f56fbddcb
Babak <basadi@gmail.com>
2024-03-05 09:36:59 +0200

    added api cost indicator

--- text2speech.py
+++ text2speech.py
@@ -31,6 +31,10 @@ def main():
   if not user_file_name:
     user_file_name = "speech.mp3"
 
+  cost = 15.00 / 1000000 * len(user_input)
+  string_cost = "Cost $" + str(cost)
+  st.write(f":grey[{string_cost}]")
+
   # Convert button
   if st.button("Convert to Speech"):
     speech_file_path = text_to_speech(user_input, voice_type)
